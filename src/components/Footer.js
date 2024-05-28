9f4df52bbe357da6c40d746483803cb6760b0f67
moali34 <mohamedaliabdu876@gmail.com>
2024-05-28 12:18:09 +0300

    png change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,10 +14,10 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3></h3>
+          <h3>Developed by Mohamed Babiker</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} MB</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
