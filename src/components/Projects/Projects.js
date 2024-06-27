e7c4c5bb8644d977fc626b1d18ab5e165fd841f9
moali34 <mohamedaliabdu876@gmail.com>
2024-06-27 03:49:42 +0300

    changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -32,16 +32,6 @@ function Projects() {
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
