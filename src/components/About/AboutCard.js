ee977595c18653559ed4f62d22b79b75be4dd5b5
moali34 <mohamedaliabdu876@gmail.com>
2024-07-06 02:10:23 +0300

    word

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -23,7 +23,7 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Community work through Discord
+              <ImPointRight /> Community work
             </li>
             <li className="about-activity">
               <ImPointRight /> Travling
